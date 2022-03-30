02b16ab8d314f4eb854845596b49649ed1ae6ba6
vladandrei228 <58209761+vladandrei228@users.noreply.github.com>
2022-03-30 19:58:36 +0200

    some changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,13 +6,20 @@ import { Router } from '@angular/router';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor(private router: Router) {}
 
   nextPage(){
     this.router.navigate(['zone-page'])
   }
+
+  ngOnInit() {
+  }
+
+  isValue: number = 0;
+
+  toggle(num) { this.isValue = num; }
 }
 
 
